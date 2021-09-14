4b439e9ba5d5b5d5c490e7b92546e4ee2cffbaf4
Liu Xue Yan <liu_xue_yan@foxmail.com>
2021-09-14 14:40:14 +0800

    add pg lock level argument

--- src/sqlalchemy_dlock/impl/postgresql.py
+++ src/sqlalchemy_dlock/impl/postgresql.py
@@ -16,17 +16,42 @@ INT64_MIN = -2**63  # min of signed int64: -2**63(-0x8000_0000_0000_0000)
 
 SLEEP_INTERVAL_DEFAULT = 1
 
-LOCK = text(dedent('''
-SELECT pg_advisory_lock(:key)
-''').strip())
+STATEMENTS = {
+    'session': {
+        'lock': text(dedent('''
+            SELECT pg_advisory_lock(:key)
+            ''').strip()),
+        'trylock': text(dedent('''
+            SELECT pg_try_advisory_lock(:key)
+            ''').strip()),
+        'unlock': text(dedent('''
+            SELECT pg_advisory_unlock(:key)
+            ''').strip()),
+    },
+    'shared': {
+        'lock': text(dedent('''
+            SELECT pg_advisory_lock_shared(:key)
+            ''').strip()),
+        'trylock': text(dedent('''
+            SELECT pg_try_advisory_lock_shared(:key)
+            ''').strip()),
+        'unlock': text(dedent('''
+            SELECT pg_advisory_unlock_shared(:key)
+            ''').strip()),
+    },
+    'transaction': {
+        'lock': text(dedent('''
+            SELECT pg_advisory_xact_lock(:key)
+            ''').strip()),
+        'trylock': text(dedent('''
+            SELECT pg_try_advisory_xact_lock(:key)
+            ''').strip()),
+        'unlock': text(dedent('''
+            SELECT pg_advisory_xact_unlock(:key)
+            ''').strip()),
+    },
+}
 
-TRY_LOCK = text(dedent('''
-SELECT pg_try_advisory_lock(:key)
-''').strip())
-
-UNLOCK = text(dedent('''
-SELECT pg_advisory_unlock(:key)
-''').strip())
 
 TConvertFunction = Callable[[Any], int]
 
@@ -64,6 +89,7 @@ class SessionLevelLock(AbstractSessionLevelLock):
                  *,
                  convert: Optional[TConvertFunction] = None,
                  interval: Union[float, int, None] = None,
+                 level: Optional[str] = None,
                  **_
                  ):
         """
@@ -85,6 +111,17 @@ class SessionLevelLock(AbstractSessionLevelLock):
                 # do something ...
                 return integer
 
+        The ``level`` argument should be one of:
+
+        - ``"session"`` (Omitted): locks an application-defined resource. 
+            If another session already holds a lock on the same resource identifier, this function will wait until the resource becomes available.
+            The lock is exclusive. Multiple lock requests stack, so that if the same resource is locked three times it must then be unlocked three times to be released for other sessions' use.
+
+        - ``"shared"``: works the same as session level lock, except the lock can be shared with other sessions requesting shared locks.
+            Only would-be exclusive lockers are locked out.
+
+        - ``"transaction"``: works the same as session level lock, except the lock is automatically released at the end of the current transaction and cannot be released explicitly.
+
         .. tip::
 
             PostgreSQL's advisory lock has no timeout mechanism in itself.
@@ -99,6 +136,9 @@ class SessionLevelLock(AbstractSessionLevelLock):
             key = default_convert(key)
         #
         self._interval = SLEEP_INTERVAL_DEFAULT if interval is None else interval
+        if level is None:
+            level = 'session'
+        self._stmt_dict = STATEMENTS[level]
         #
         super().__init__(connection_or_session, key)
 
@@ -114,9 +154,9 @@ class SessionLevelLock(AbstractSessionLevelLock):
         if block:
             if timeout is None:
                 # None: set the timeout period to infinite.
-                stmt = LOCK.params(key=self._key)
+                stmt = self._stmt_dict['lock'].params(key=self._key)
                 r = self.connection_or_session.execute(stmt)
-                deque(r.all(), maxlen=0)
+                deque(r, maxlen=0)
                 self._acquired = True
             else:
                 # negative value for `timeout` are equivalent to a `timeout` of zero.
@@ -125,10 +165,10 @@ class SessionLevelLock(AbstractSessionLevelLock):
                 interval = self._interval if interval is None else interval
                 if interval < 0:
                     raise ValueError('interval must not be smaller than 0')
-                stmt = TRY_LOCK.params(key=self._key)
+                stmt = self._stmt_dict['trylock'].params(key=self._key)
                 ts_begin = time()
                 while True:
-                    ret_val = self.connection_or_session.execute(stmt).scalar()
+                    ret_val = self.connection_or_session.execute(stmt).scalar_one()
                     if ret_val:  # succeed
                         self._acquired = True
                         break
@@ -138,8 +178,8 @@ class SessionLevelLock(AbstractSessionLevelLock):
         else:
             # This will either obtain the lock immediately and return true,
             # or return false without waiting if the lock cannot be acquired immediately.
-            stmt = TRY_LOCK.params(key=self._key)
-            ret_val = self.connection_or_session.execute(stmt).scalar()
+            stmt = self._stmt_dict['trylock'].params(key=self._key)
+            ret_val = self.connection_or_session.execute(stmt).scalar_one()
             self._acquired = bool(ret_val)
         #
         return self._acquired
@@ -147,8 +187,8 @@ class SessionLevelLock(AbstractSessionLevelLock):
     def release(self, **_):
         if not self._acquired:
             raise ValueError('invoked on an unlocked lock')
-        stmt = UNLOCK.params(key=self._key)
-        ret_val = self.connection_or_session.execute(stmt).scalar()
+        stmt = self._stmt_dict['unlock'].params(key=self._key)
+        ret_val = self.connection_or_session.execute(stmt).scalar_one()
         if ret_val:
             self._acquired = False
         else:
