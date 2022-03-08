87a2876ba78c9e604411b1cb679de3d28029de60
Liu Xue Yan <liu_xue_yan@foxmail.com>
2022-03-08 11:41:08 +0800

    async pg simple test ok

--- src/sqlalchemy_dlock/asyncio/impl/postgresql.py
+++ src/sqlalchemy_dlock/asyncio/impl/postgresql.py
@@ -1,11 +1,11 @@
 import asyncio
 from collections import deque
+from hashlib import blake2b
 from sys import byteorder
 from textwrap import dedent
-from time import sleep, time
+from time import time
 from typing import Any, Callable, Optional, Union
 
-import libscrc
 from sqlalchemy import text
 
 from ...exceptions import SqlAlchemyDLockDatabaseError
@@ -17,29 +17,54 @@ INT64_MIN = -2**63  # min of signed int64: -2**63(-0x8000_0000_0000_0000)
 
 SLEEP_INTERVAL_DEFAULT = 1
 
-LOCK = text(dedent('''
-SELECT pg_advisory_lock(:key)
-''').strip())
+STATEMENTS = {
+    'session': {
+        'lock': text(dedent('''
+            SELECT pg_advisory_lock(:key)
+            ''').strip()),
+        'trylock': text(dedent('''
+            SELECT pg_try_advisory_lock(:key)
+            ''').strip()),
+        'unlock': text(dedent('''
+            SELECT pg_advisory_unlock(:key)
+            ''').strip()),
+    },
+    'shared': {
+        'lock': text(dedent('''
+            SELECT pg_advisory_lock_shared(:key)
+            ''').strip()),
+        'trylock': text(dedent('''
+            SELECT pg_try_advisory_lock_shared(:key)
+            ''').strip()),
+        'unlock': text(dedent('''
+            SELECT pg_advisory_unlock_shared(:key)
+            ''').strip()),
+    },
+    'transaction': {
+        'lock': text(dedent('''
+            SELECT pg_advisory_xact_lock(:key)
+            ''').strip()),
+        'trylock': text(dedent('''
+            SELECT pg_try_advisory_xact_lock(:key)
+            ''').strip()),
+        'unlock': text(dedent('''
+            SELECT pg_advisory_xact_unlock(:key)
+            ''').strip()),
+    },
+}
 
-TRY_LOCK = text(dedent('''
-SELECT pg_try_advisory_lock(:key)
-''').strip())
-
-UNLOCK = text(dedent('''
-SELECT pg_advisory_unlock(:key)
-''').strip())
 
 TConvertFunction = Callable[[Any], int]
 
 
-def default_convert(key: Union[bytearray, bytes, str]) -> int:
+def default_convert(key: Union[bytearray, bytes, memoryview, str, int]) -> int:
     if isinstance(key, str):
         key = key.encode()
-    if isinstance(key, (bytearray, bytes)):
-        result = libscrc.iso(key)  # type: ignore
-    else:
-        raise TypeError('{}'.format(type(key)))
-    return ensure_int64(result)
+    if isinstance(key, (bytearray, bytes, memoryview)):
+        return int.from_bytes(blake2b(key, digest_size=8).digest(), byteorder, signed=True)
+    elif isinstance(key, int):
+        return ensure_int64(key)
+    raise TypeError('{}'.format(type(key)))
 
 
 def ensure_int64(i: int) -> int:
@@ -65,6 +90,7 @@ class SessionLevelLock(AbstractSessionLevelLock):
                  *,
                  convert: Optional[TConvertFunction] = None,
                  interval: Union[float, int, None] = None,
+                 level: Optional[str] = None,
                  **_
                  ):
         """
@@ -86,6 +112,17 @@ class SessionLevelLock(AbstractSessionLevelLock):
                 # do something ...
                 return integer
 
+        The ``level`` argument should be one of:
+
+        - ``"session"`` (Omitted): locks an application-defined resource.
+            If another session already holds a lock on the same resource identifier, this function will wait until the resource becomes available.
+            The lock is exclusive. Multiple lock requests stack, so that if the same resource is locked three times it must then be unlocked three times to be released for other sessions' use.
+
+        - ``"shared"``: works the same as session level lock, except the lock can be shared with other sessions requesting shared locks.
+            Only would-be exclusive lockers are locked out.
+
+        - ``"transaction"``: works the same as session level lock, except the lock is automatically released at the end of the current transaction and cannot be released explicitly.
+
         .. tip::
 
             PostgreSQL's advisory lock has no timeout mechanism in itself.
@@ -100,6 +137,8 @@ class SessionLevelLock(AbstractSessionLevelLock):
             key = default_convert(key)
         #
         self._interval = SLEEP_INTERVAL_DEFAULT if interval is None else interval
+        self._level = level or 'session'
+        self._stmt_dict = STATEMENTS[self._level]
         #
         super().__init__(connection_or_session, key)
 
@@ -115,9 +154,9 @@ class SessionLevelLock(AbstractSessionLevelLock):
         if block:
             if timeout is None:
                 # None: set the timeout period to infinite.
-                stmt = LOCK.params(key=self._key)
+                stmt = self._stmt_dict['lock'].params(key=self._key)
                 r = await self.connection_or_session.execute(stmt)
-                deque(r.all(), maxlen=0)
+                deque(r, maxlen=0)
                 self._acquired = True
             else:
                 # negative value for `timeout` are equivalent to a `timeout` of zero.
@@ -126,7 +165,7 @@ class SessionLevelLock(AbstractSessionLevelLock):
                 interval = self._interval if interval is None else interval
                 if interval < 0:
                     raise ValueError('interval must not be smaller than 0')
-                stmt = TRY_LOCK.params(key=self._key)
+                stmt = self._stmt_dict['trylock'].params(key=self._key)
                 ts_begin = time()
                 while True:
                     r = await self.connection_or_session.execute(stmt)
@@ -140,7 +179,7 @@ class SessionLevelLock(AbstractSessionLevelLock):
         else:
             # This will either obtain the lock immediately and return true,
             # or return false without waiting if the lock cannot be acquired immediately.
-            stmt = TRY_LOCK.params(key=self._key)
+            stmt = self._stmt_dict['trylock'].params(key=self._key)
             r = await self.connection_or_session.execute(stmt)
             ret_val = r.scalar_one()
             self._acquired = bool(ret_val)
@@ -150,7 +189,7 @@ class SessionLevelLock(AbstractSessionLevelLock):
     async def release(self, **_):
         if not self._acquired:
             raise ValueError('invoked on an unlocked lock')
-        stmt = UNLOCK.params(key=self._key)
+        stmt = self._stmt_dict['unlock'].params(key=self._key)
         r = await self.connection_or_session.execute(stmt)
         ret_val = r.scalar_one()
         if ret_val:
@@ -159,3 +198,7 @@ class SessionLevelLock(AbstractSessionLevelLock):
             self._acquired = False
             raise SqlAlchemyDLockDatabaseError(
                 'The advisory lock "{}" was not held.'.format(self._key))
+
+    @property
+    def level(self) -> str:
+        return self._level
