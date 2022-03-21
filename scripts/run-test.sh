2c51951a54597ed79facd5d2fb4c2aaf09a7b10f
Liu Xue Yan <liu_xue_yan@foxmail.com>
2022-03-21 13:11:20 +0800

    optimize testing scripts

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -3,12 +3,12 @@
 set -e
 
 ENV_DIR="$(mktemp -d)"
-trap '{ rm -fr "${ENV_DIR}" ; }' EXIT
-echo "ENV_DIR: ${ENV_DIR}"
+trap '{ echo "Remove virtual Python environment: \"${ENV_DIR}\""; rm -fr "${ENV_DIR}"; }' EXIT
 
+echo "Create virtual Python environment: \"${ENV_DIR}\""
 python -m venv ${ENV_DIR}
 
-${ENV_DIR}/bin/pip install -U --no-cache-dir pip setuptools wheel \
+${ENV_DIR}/bin/pip install -U --no-cache-dir pip \
 $(if [ -n "${PYPI_INDEX_URL}" ]; then echo "-i ${PYPI_INDEX_URL}"; fi)
 
 if [ -n "${PYPI_INDEX_URL}" ]
