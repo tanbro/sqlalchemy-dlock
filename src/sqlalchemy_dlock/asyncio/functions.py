c09795d0487f38c4e4465f3f60ba141ea442d77f
tanbro <tanbro@outlook.com>
2023-06-17 00:00:39 +0800

    optimize asyncio

--- src/sqlalchemy_dlock/asyncio/functions.py
+++ src/sqlalchemy_dlock/asyncio/functions.py
@@ -1,5 +1,6 @@
 from importlib import import_module
 
+from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import AsyncConnection
 
 from ..utils import safe_name
@@ -8,14 +9,19 @@ from .types import BaseAsyncSadLock, TAsyncConnectionOrSession
 __all__ = ["create_async_sadlock"]
 
 
-def create_async_sadlock(connection_or_engine: TAsyncConnectionOrSession, key, *args, **kwargs) -> BaseAsyncSadLock:
-    if isinstance(connection_or_engine, AsyncConnection):
-        name = safe_name(connection_or_engine.sync_engine.engine.name)
+def create_async_sadlock(connection_or_session: TAsyncConnectionOrSession, key, *args, **kwargs) -> BaseAsyncSadLock:
+    if isinstance(connection_or_session, AsyncConnection):
+        sync_engine = connection_or_session.sync_engine
     else:
-        name = safe_name(connection_or_engine.get_bind().name)
+        bind = connection_or_session.get_bind()
+        if isinstance(bind, Connection):
+            sync_engine = bind.engine
+        else:
+            sync_engine = bind
+    engine_name = safe_name(sync_engine.name)
     try:
-        mod = import_module("..impl.{}".format(name), __name__)
+        mod = import_module("..impl.{}".format(engine_name), __name__)
     except ImportError as exception:  # pragma: no cover
-        raise NotImplementedError("{}: {}".format(name, exception))
+        raise NotImplementedError("{}: {}".format(engine_name, exception))
     lock_cls = getattr(mod, "AsyncSadLock")
-    return lock_cls(connection_or_engine, key, *args, **kwargs)
+    return lock_cls(connection_or_session, key, *args, **kwargs)
