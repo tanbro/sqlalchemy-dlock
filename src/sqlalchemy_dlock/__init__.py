db649a36ba5e4935f87275fe23c17fcc7a10fd78
Liu Xue Yan <liu_xue_yan@foxmail.com>
2024-08-27 18:43:47 +0800

    ci(lint): Update linting tool configurations, ignore specific folders
    - Updated the .pre-commit-config.yaml file, upgrading the versions of ruff and mypy to the latest. Added the ignore_patterns parameter to the mypy configuration to ignore the tests directory.
    - Also updated the version of .jsonschema-checks.
    In the .ruff.toml file, enabled ignoring F401 errors for all __init__.py files.
    - Modified the pyproject.toml file, updating the requires-python version and categories.
    - Finally, cleaned up code imports in __init__.py and asyncio/__init__.py files.

--- src/sqlalchemy_dlock/__init__.py
+++ src/sqlalchemy_dlock/__init__.py
@@ -3,5 +3,5 @@ Distributed lock based on Database and SQLAlchemy
 """
 
 from ._version import __version__, __version_tuple__
-from .exceptions import *  # noqa: F403
-from .factory import *  # noqa: F403
+from .exceptions import SqlAlchemyDLockBaseException, SqlAlchemyDLockDatabaseError
+from .factory import create_sadlock
--- tests/asyncio/__init__.py
+++ tests/asyncio/__init__.py
@@ -4,4 +4,4 @@ import platform
 # Psycopg cannot use the 'ProactorEventLoop' to run in async mode.
 # use a compatible event loop, for instance by setting 'asyncio.set_event_loop_policy(WindowsSelectorEventLoopPolicy())'
 if platform.system() == "Windows":
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy()) # type: ignore[attr-defined]
